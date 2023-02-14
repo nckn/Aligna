24b32c4a1b42986fe993683f52f98f1fd55bcbfe
Niels Konrad <mail@konradstudio.com>
2023-02-14 21:21:16 +0100

    modifying script

--- code.ts
+++ code.ts
@@ -14,17 +14,39 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+ 
+  // if (msg.type === 'create-guide-line') {
+  // org code - start
+  // if (msg.type === 'create-rectangles') {
+  //   const nodes: SceneNode[] = [];
+  //   for (let i = 0; i < msg.count; i++) {
+  //     const rect = figma.createRectangle();
+  //     rect.x = i * 150;
+  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+  //     figma.currentPage.appendChild(rect);
+  //     nodes.push(rect);
+  //   }
+  //   figma.currentPage.selection = nodes;
+  //   figma.viewport.scrollAndZoomIntoView(nodes);
+  // }
+  // org code - end
+
+  if (msg.type === 'create-guide-line') {
+    const line = figma.createLine()
+
+    // Move to (50, 50)
+    line.x = -2000
+    line.y = 50
+
+    // Make line 200px long
+    // line.resize(200, 0)
+    // line.resize(figma.viewport.bounds.width, 0)
+    line.resize(figma.viewport.bounds.width * 10, 0)
+
+    // 4px thick red line with arrows at each end
+    line.strokeWeight = 1
+    line.strokes = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }]
+    // line.strokeCap = 'ARROW_LINES'
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
